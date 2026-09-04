55e93405d5fb07a4cc00031827b158e2f71627c3
tanmay-civilsih <tanmaybose18@gmail.com>
2026-09-04 19:40:36 +0530

    feat: expose registry domain

---
+++ floodguard/registry/__init__.py
@@ -0,0 +1,5 @@
+"""FloodGuard-AI data source registry domain."""
+
+from floodguard.registry.contracts import AccessClass, SourceCategory, SourceStatus
+
+__all__ = ["AccessClass", "SourceCategory", "SourceStatus"]
